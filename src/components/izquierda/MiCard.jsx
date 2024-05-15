780a2e19fd8004c0bceb321ecea1d888f30164d5
Alan Ribeca <alanribeca2003@gmail.com>
2024-05-15 11:13:08 -0300

    creando la estructura de las card

--- src/components/izquierda/MiCard.jsx
+++ src/components/izquierda/MiCard.jsx
@@ -3,7 +3,22 @@ import "./micard.scss";
 export const MiCard = () => {
   return (
     <section className="miCard">
-        <h1>hola</h1>
+      <div className="containerImg">
+        <div className="imgPortada"></div>
+        <div className="imgPersonal"></div>
+      </div>
+      <h2 className="nombre">Alan Ribeca</h2>
+      <p className="habilidades">
+        Desarrollardor web -- Front End | HTML5 | CSS | JavaScript | React...
+      </p>
+      <hr className="hrCard" />
+      <div className="containerSpan">
+        <span className="visualizaciones">Visualizaciones del perfil <span>500</span> </span>
+        <span className="visualizaciones">Impresiones de la publicación <span>+999</span> </span>
+      </div>
+      <hr className="hrCard"/>
+      <p className="textoPremium">Alcanza tus objetivos profesionales con Premium</p>
+      
     </section>
-  )
-}
+  );
+};
