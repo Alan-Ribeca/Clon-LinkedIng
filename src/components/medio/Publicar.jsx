f73ff3533d9f069dabcb92fcd2cc83c63e359e09
Alan Ribeca <alanribeca2003@gmail.com>
2024-05-22 08:21:14 -0300

    listo la parte de publicar

--- src/components/medio/Publicar.jsx
+++ src/components/medio/Publicar.jsx
@@ -14,8 +14,8 @@ export const Publicar = () => {
           </div>
           <input
             type="text"
-            placeholder="Crear publicaion"
-            className="inpitEscribir"
+            placeholder="Crear publicación"
+            className="inputEscribir"
           />
         </div>
         <div className="contenido">
@@ -26,6 +26,7 @@ export const Publicar = () => {
                 viewBox="0 0 24 24"
                 width="24"
                 height="24"
+                className="primerSvg"
               >
                 <path d="M19 4H5a3 3 0 00-3 3v10a3 3 0 003 3h14a3 3 0 003-3V7a3 3 0 00-3-3zm1 13a1 1 0 01-.29.71L16 14l-2 2-6-6-4 4V7a1 1 0 011-1h14a1 1 0 011 1zm-2-7a2 2 0 11-2-2 2 2 0 012 2z"></path>
               </svg>
@@ -38,6 +39,7 @@ export const Publicar = () => {
                 viewBox="0 0 24 24"
                 width="24"
                 height="24"
+                className="segundoSvg"
               >
                 <path d="M3 3v15c0 1.66 1.34 3 3 3h12c1.66 0 3-1.34 3-3V3H3zm13 1.75a1.25 1.25 0 110 2.5 1.25 1.25 0 010-2.5zm-8 0a1.25 1.25 0 110 2.5 1.25 1.25 0 010-2.5zM19 18c0 .55-.45 1-1 1H6c-.55 0-1-.45-1-1V9h14v9zM7 11h2v2H7v-2zm0 4h2v2H7v-2zm4-4h2v2h-2v-2zm0 4h2v2h-2v-2zm4-4h2v2h-2v-2zm0 4h2v2h-2v-2z"></path>
               </svg>
@@ -49,6 +51,7 @@ export const Publicar = () => {
                 viewBox="0 0 24 24"
                 width="24"
                 height="24"
+                className="tercerSvg"
               >
                 <path d="M21 3v2H3V3zm-6 6h6V7h-6zm0 4h6v-2h-6zm0 4h6v-2h-6zM3 21h18v-2H3zM13 7H3v10h10z"></path>
               </svg>
