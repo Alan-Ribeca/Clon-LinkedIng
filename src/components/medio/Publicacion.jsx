f509dd852ed5ee9438ed7e86b368ae66de467fbd
Alan Ribeca <alanribeca2003@gmail.com>
2024-05-23 10:39:14 -0300

    mejorando la estructura de las publicaciones

--- src/components/medio/Publicacion.jsx
+++ src/components/medio/Publicacion.jsx
@@ -9,6 +9,9 @@ export const Publicacion = () => {
       hora: "10 horas",
       titulo: "La confianza en la nube y en Google, ahora queda en duda.",
       imgPublicada: "./img/proyecFamily.png1",
+      mg: "13",
+      comentario: "",
+      compartido: "",
       id: 1,
     },
     {
@@ -20,6 +23,9 @@ export const Publicacion = () => {
       Inscríbete gratis ingresando al siguiente link: https://lnkd.in/dSYZngvS
       ¡Te esperamos! 🙌"`,
       imgPublicada: "./img/educationItImg.jpg",
+      mg: "13",
+      comentario: "",
+      compartido: "",
       id: 2,
     },
     {
@@ -29,7 +35,9 @@ export const Publicacion = () => {
       hora: "10 horas",
       titulo: "Quedan pocos días de la #FeriaDeCursosGratis 🎡",
       imgPublicada: "./img/proyecFamily.png1",
-
+      mg: "13",
+      comentario: "",
+      compartido: "",
       id: 3,
     },
     {
@@ -40,7 +48,9 @@ export const Publicacion = () => {
       titulo:
         "CSS Flexbox is a powerful tool that can help you create appealing and responsive webpage layouts.",
       imgPublicada: "./img/proyecFamily.png1",
-
+      mg: "13",
+      comentario: "",
+      compartido: "",
       id: 4,
     },
   ];
@@ -55,6 +65,9 @@ export const Publicacion = () => {
           titulo,
           imgPublicada,
           timeLenctura,
+          mg,
+          comentario,
+          compartido,
           id,
         }) => (
           <div key={id} className="publicacionContainer">
@@ -68,6 +81,18 @@ export const Publicacion = () => {
                 <spanp className="descripcion">{descripcion}</spanp>
                 <span className="hora">{hora}</span>
               </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  role="none"
+                  className="opciones"
+                >
+                  <path d="M3 9.5A1.5 1.5 0 114.5 8 1.5 1.5 0 013 9.5zM11.5 8A1.5 1.5 0 1013 6.5 1.5 1.5 0 0011.5 8zm-5 0A1.5 1.5 0 108 6.5 1.5 1.5 0 006.5 8z"></path>
+                </svg>
             </div>
             <p className="titlePublic">{titulo}</p>
             <div className="imgPublicada">
@@ -78,9 +103,33 @@ export const Publicacion = () => {
             </div>
             <div className="reacciones">
               <div className="detalles">
-                <span className="mg">1</span>
-                <span className="comentario">2 comentarios</span>
-                <span className="compartido">4 veces compartido</span>
+                <span className="mg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                  >
+                    <g>
+                      <circle cx="8" cy="8" r="7" fill="#378fe9" />
+                      <path
+                        d="M11.93 7.25h-.55c-.05 0-.15-.19-.4-.46-.37-.4-.78-.91-1.07-1.19a7.13 7.13 0 01-1.73-2.24c-.24-.51-.26-.74-.75-.74a.78.78 0 00-.67.81c0 .14.07.63.1.8a7.54 7.54 0 001 2.2H4.12a.88.88 0 00-.65.28.84.84 0 00-.23.66.91.91 0 00.93.85h.16a.82.82 0 00-.55.24.77.77 0 00-.21.54.81.81 0 00.74.8.8.8 0 00.33 1.42.76.76 0 00-.09.55.87.87 0 00.85.63h2.29a3.8 3.8 0 00.89-.11l1.42-.4h1.9c1.02-.04 1.29-4.64.03-4.64z"
+                        fill="#d0e8ff"
+                        fillRule="evenodd"
+                      />
+                      <path
+                        d="M7.43 6.43H4.11a.88.88 0 00-.88 1 .92.92 0 00.93.84h.16a.82.82 0 00-.55.24.77.77 0 00-.21.56.83.83 0 00.74.81.81.81 0 00-.31.63.81.81 0 00.65.8.78.78 0 00-.09.56.86.86 0 00.85.62h2.29a3.8 3.8 0 00.89-.11l1.42-.47h1.9c1 0 1.27-4.64 0-4.64a5 5 0 01-.55 0s-.15-.19-.4-.46h0c-.37-.4-.78-.91-1.07-1.19a7.08 7.08 0 01-1.7-2.25 2.14 2.14 0 00-.32-.52.83.83 0 00-1.16.09 1.39 1.39 0 00-.25.38 1.71 1.71 0 00-.09.3 2.38 2.38 0 00.07.84 4.12 4.12 0 00.27.84 6.65 6.65 0 00.66 1 .18.18 0 01.07.08"
+                        fill="none"
+                        stroke="#004182"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </g>
+                  </svg>
+                  {mg}
+                </span>
+                <span className="comentario">{comentario}</span>
+                <span className="compartido">{compartido}</span>
               </div>
               <hr />
               <div className="abajo">
