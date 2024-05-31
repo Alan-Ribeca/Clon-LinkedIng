4f64e64c116b56573d05177922aa7a362fd54ba5
Alan Ribeca <alanribeca2003@gmail.com>
2024-05-31 07:51:49 -0300

    siguiendo con el responsive

--- src/components/navbar/Navegacion.jsx
+++ src/components/navbar/Navegacion.jsx
@@ -17,7 +17,7 @@ export const Navegacion = () => {
           >
             <path d="M23 9v2h-2v7a3 3 0 01-3 3h-4v-6h-4v6H6a3 3 0 01-3-3v-7H1V9l11-7 5 3.18V2h3v5.09z"></path>
           </svg>
-          Inicio
+          <span className="nombreLogo">Inicio</span>
         </li>
         <li className="liNav">
           <svg
@@ -32,7 +32,7 @@ export const Navegacion = () => {
           >
             <path d="M12 16v6H3v-6a3 3 0 013-3h3a3 3 0 013 3zm5.5-3A3.5 3.5 0 1014 9.5a3.5 3.5 0 003.5 3.5zm1 2h-2a2.5 2.5 0 00-2.5 2.5V22h7v-4.5a2.5 2.5 0 00-2.5-2.5zM7.5 2A4.5 4.5 0 1012 6.5 4.49 4.49 0 007.5 2z"></path>
           </svg>
-          Mi red
+          <span className="nombreLogo">Mi red</span>
         </li>
         <li className="liNav">
           <svg
@@ -47,7 +47,7 @@ export const Navegacion = () => {
           >
             <path d="M17 6V5a3 3 0 00-3-3h-4a3 3 0 00-3 3v1H2v4a3 3 0 003 3h14a3 3 0 003-3V6zM9 5a1 1 0 011-1h4a1 1 0 011 1v1H9zm10 9a4 4 0 003-1.38V17a3 3 0 01-3 3H5a3 3 0 01-3-3v-4.38A4 4 0 005 14z"></path>
           </svg>
-          Empleos
+          <span className="nombreLogo">Empleos</span>
         </li>
         <li className="liNav">
           <svg
@@ -62,7 +62,7 @@ export const Navegacion = () => {
           >
             <path d="M16 4H8a7 7 0 000 14h4v4l8.16-5.39A6.78 6.78 0 0023 11a7 7 0 00-7-7zm-8 8.25A1.25 1.25 0 119.25 11 1.25 1.25 0 018 12.25zm4 0A1.25 1.25 0 1113.25 11 1.25 1.25 0 0112 12.25zm4 0A1.25 1.25 0 1117.25 11 1.25 1.25 0 0116 12.25z"></path>
           </svg>
-          Mensajes
+          <span className="nombreLogo">Mensajes</span>
         </li>
         <li className="liNav">
           <svg
@@ -77,7 +77,7 @@ export const Navegacion = () => {
           >
             <path d="M22 19h-8.28a2 2 0 11-3.44 0H2v-1a4.52 4.52 0 011.17-2.83l1-1.17h15.7l1 1.17A4.42 4.42 0 0122 18zM18.21 7.44A6.27 6.27 0 0012 2a6.27 6.27 0 00-6.21 5.44L5 13h14z"></path>
           </svg>
-          Notificaciones
+          <span className="nombreLogo">Notificaciones</span>
         </li>
         <li className="liNav">
           <img
@@ -86,7 +86,7 @@ export const Navegacion = () => {
             className="imgAvatar"
           />
           <div className="divAvatar">
-            Yo
+            <span className="nombreLogo">Yo</span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="13"
@@ -113,7 +113,7 @@ export const Navegacion = () => {
             <path d="M3 3h4v4H3zm7 4h4V3h-4zm7-4v4h4V3zM3 14h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4zM3 21h4v-4H3zm7 0h4v-4h-4zm7 0h4v-4h-4z"></path>
           </svg>
           <div className="divNegocio">
-            Para negocios
+            <span className="nombreLogo">Para negocios</span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="13"
