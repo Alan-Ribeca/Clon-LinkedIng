f01ae72b9374e3c365e29550f096ebce89191161
Alan Ribeca <alanribeca2003@gmail.com>
2024-06-04 22:28:14 -0300

    en movil

--- src/components/navbar/Movil.jsx
+++ src/components/navbar/Movil.jsx
@@ -2,41 +2,39 @@ import "./movil.scss";
 
 export const Movil = () => {
   return (
-    <div className="containerMovil">
-      <img
-        src="./img/avatare.jpg"
-        alt="foto de avatar predeterminada"
-        className="imgAvatar"
-      />
-
-      <div className="buscadorCelu">
-        <input type="text" placeholder="Buscar" className="inputBuscador" />
+    <>
+      <div className="containerMovil">
+        <img
+          src="./img/avatare.jpg"
+          alt="foto de avatar predeterminada"
+          className="imgAvatar"
+        />
+        <div className="buscadorCelu">
+          <input type="text" placeholder="Buscar" className="inputBuscador" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="13"
+            height="13"
+            fill="currentColor"
+            className="BuscadorM"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+          </svg>
+        </div>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="13"
-          height="13"
+          viewBox="0 0 24 24"
+          data-supported-dps="24x24"
           fill="currentColor"
-          className="BuscadorM"
-          viewBox="0 0 16 16"
+          className="logos mensaje"
+          width="24"
+          height="24"
+          focusable="false"
         >
-          <path
-            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"
-          />
+          <path d="M16 4H8a7 7 0 000 14h4v4l8.16-5.39A6.78 6.78 0 0023 11a7 7 0 00-7-7zm-8 8.25A1.25 1.25 0 119.25 11 1.25 1.25 0 018 12.25zm4 0A1.25 1.25 0 1113.25 11 1.25 1.25 0 0112 12.25zm4 0A1.25 1.25 0 1117.25 11 1.25 1.25 0 0116 12.25z"></path>
         </svg>
       </div>
-
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        data-supported-dps="24x24"
-        fill="currentColor"
-        className="logos mensaje"
-        width="24"
-        height="24"
-        focusable="false"
-      >
-        <path d="M16 4H8a7 7 0 000 14h4v4l8.16-5.39A6.78 6.78 0 0023 11a7 7 0 00-7-7zm-8 8.25A1.25 1.25 0 119.25 11 1.25 1.25 0 018 12.25zm4 0A1.25 1.25 0 1113.25 11 1.25 1.25 0 0112 12.25zm4 0A1.25 1.25 0 1117.25 11 1.25 1.25 0 0116 12.25z"></path>
-      </svg>
-    </div>
+    </>
   );
 };
